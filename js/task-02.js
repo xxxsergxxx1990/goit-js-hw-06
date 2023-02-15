6717d650b2c193ee6af48be0e6d32dfd074bf73f
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-02-15 20:51:09 +0200

    1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const allIngredients = document.querySelector('#ingredients')
+
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items = document.createElement('li');
+  items.textContent = ingredient;
+  items.classList = 'item'
+  allIngredients.append(items)
+})
+console.log(allIngredients)
