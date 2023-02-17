fe3756808e319b567a75a55f055f3be1548f1ddd
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-02-17 12:44:49 +0200

    2

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener('submit',onFormSubmit);
+
+const profile = {};
+
+
+function onFormSubmit(event){
+event.preventDefault();
+
+
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+
+    if (email.value === '' || password.value === '') {
+    alert('всі поля повинні бути заповнені!');
+  } else {
+    profile.email = email.value;
+    profile.password = password.value;
+
+    console.log(profile);
+  }
+  event.currentTarget.reset();
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const colorText = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.body;
+
+const colorChange = () => {
+  colorText.textContent =  getRandomHexColor();
+  body.style.backgroundColor =  colorText.textContent;
+};
+
+btnEl.addEventListener('click', colorChange);
